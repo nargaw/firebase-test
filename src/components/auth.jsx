125e817a1196c385d3cb59410d44177c11c88975
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-12-30 05:15:27 -0500

    remove all files in .gitignore

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,41 +1,45 @@
 import { useState } from 'react';
 import { auth, googleProvider } from '../config/firebase.js'
 import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 
 export const Auth = () => {
 
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+    console.log(firebaseui)
+    // const [email, setEmail] = useState()
+    // const [password, setPassword] = useState()
 
-    console.log(auth?.currentUser?.email)
+    // console.log(auth?.currentUser?.email)
 
-    const signIn = async () => {
-        try {
-            await createUserWithEmailAndPassword(auth, email, password)
-        } catch (err){
-            console.error(err)
-        }
-    };
+    // const signIn = async () => {
+    //     try {
+    //         await createUserWithEmailAndPassword(auth, email, password)
+    //     } catch (err){
+    //         console.error(err)
+    //     }
+    // };
 
-    const signInWithGoogle = async () => {
-        try {
-            await signInWithPopup(auth, googleProvider)
-        } catch (err){
-            console.error(err)
-        }
-    };
+    // const signInWithGoogle = async () => {
+    //     try {
+    //         await signInWithPopup(auth, googleProvider)
+    //     } catch (err){
+    //         console.error(err)
+    //     }
+    // };
 
-    const logOut = async () => {
-        try {
-            await signOut(auth)
-        } catch (err){
-            console.error(err)
-        }
-    };
+    // const logOut = async () => {
+    //     try {
+    //         await signOut(auth)
+    //     } catch (err){
+    //         console.error(err)
+    //     }
+    // };
 
     return (
         <>
-            <div>
+            <h1>Welcome</h1>
+            {/* <div>
                 <input placeholder="Email..." onChange={(e) => setEmail(e.target.value)}/>
                 <input
                     type="password" 
@@ -45,7 +49,7 @@ export const Auth = () => {
                 <button onClick={signIn}> Sign In</button>
                 <button onClick={signInWithGoogle}>Sign In with Google</button>
                 <button onClick={logOut}>Logout</button>
-            </div>
+            </div> */}
         </>
         
     )
